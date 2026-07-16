522b53946466661ca2601bdbf1a369e66f1a5ab2
mahith9390 <mahithkundeti@gmail.com>
2026-07-16 11:14:09 +0530

    Fix Login.jsx merge conflict

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
-import "../styles/Login.css";
-import { Link } from "react-router-dom";
-
-function Login() {
-=======
 import { useState } from "react";
-import "./../styles/login.css";
+import { Link } from "react-router-dom";
+import "../styles/Login.css";
 
 function Login({ onSignupClick }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
- const handleLogin = async () => {
-  console.log("Login button clicked");
-
-  try {
+  const handleLogin = async () => {
+    try {
       const res = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
@@ -31,9 +24,6 @@ function Login({ onSignupClick }) {
       if (res.ok) {
         alert("Login Successful");
         console.log(data);
-
-        
-        
       } else {
         alert(data.message || "Login Failed");
       }
@@ -43,7 +33,6 @@ function Login({ onSignupClick }) {
     }
   };
 
->>>>>>> e989f09 (Integrated landing page and login)
   return (
     <div className="container">
       <div className="card">
@@ -57,22 +46,16 @@ function Login({ onSignupClick }) {
         <input
           type="email"
           placeholder="Enter your email"
-<<<<<<< HEAD
-=======
           value={email}
           onChange={(e) => setEmail(e.target.value)}
->>>>>>> e989f09 (Integrated landing page and login)
         />
 
         <label>Password</label>
         <input
           type="password"
           placeholder="Enter password"
-<<<<<<< HEAD
-=======
           value={password}
           onChange={(e) => setPassword(e.target.value)}
->>>>>>> e989f09 (Integrated landing page and login)
         />
 
         <div className="options">
@@ -85,23 +68,9 @@ function Login({ onSignupClick }) {
           </Link>
         </div>
 
-<<<<<<< HEAD
-        <button className="login-btn">Login</button>
-
-        <p className="bottom-text">
-          Don't have an account?{" "}
-          <Link to="/signup">Sign Up</Link>
-        </p>
-
-        <br />
-
-        <Link to="/">
-          <button className="home-btn">
-            ⬅️ Back to Home
-          </button>
-        </Link>
-=======
-        <button onClick={handleLogin}>Login</button>
+        <button className="login-btn" onClick={handleLogin}>
+          Login
+        </button>
 
         <p className="bottom-text">
           Don't have an account?{" "}
@@ -112,7 +81,14 @@ function Login({ onSignupClick }) {
             Sign Up
           </span>
         </p>
->>>>>>> e989f09 (Integrated landing page and login)
+
+        <br />
+
+        <Link to="/">
+          <button className="home-btn">
+            ⬅️ Back to Home
+          </button>
+        </Link>
       </div>
     </div>
   );
