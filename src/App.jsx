a498221ce2794799e143c7e4043d7cd1d2e930f1
mahith9390 <mahithkundeti@gmail.com>
2026-07-16 12:26:13 +0530

    Landing page ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
 import { useState } from "react";
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import FeaturedJobs from "./components/FeaturedJobs";
+import WhyChooseUs from "./components/WhyChooseUs";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [page, setPage] = useState("home");
 
   return (
     <>
-      {isLogin ? (
-        <Login onSignupClick={() => setIsLogin(false)} />
-      ) : (
-        <Signup onLoginClick={() => setIsLogin(true)} />
+      {page === "home" && (
+        <>
+          <Header 
+            onLoginClick={() => setPage("login")}
+            onSignupClick={() => setPage("signup")}
+          />
+
+          <Hero />
+          <FeaturedJobs />
+          <WhyChooseUs />
+          <ContactSection />
+          <Footer />
+        </>
+      )}
+
+      {page === "login" && (
+        <Login 
+          onSignupClick={() => setPage("signup")}
+          onHomeClick={() => setPage("home")}
+        />
+      )}
+
+      {page === "signup" && (
+        <Signup 
+          onLoginClick={() => setPage("login")}
+          onHomeClick={() => setPage("home")}
+        />
       )}
     </>
   );
